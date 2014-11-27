a7d4c7c85843d4efabae61a7e6c6e4c32ebe0c54
Ulrich Habel <espero7757@gmx.net>
2014-11-27 20:57:18 +0000

    avr_DF4IAH_Bootloader -  schematic added

--- Src/main.c
+++ Src/main.c
@@ -290,8 +290,7 @@ int main(void)
 
 	vectortable_to_bootloader();
 	init_wdt();
-	debug_endlessTogglePin();						// XXX BLOCKING call forever, starts 327ms after power-up
-	init_usb();										// ends at 315 ms after power-up
+	init_usb();										// starts at 67 ms after power-up, ends at 316 ms after power-up
 	init_probe();									// ends at 325 ms after power-up
 	init_clkPullPwm();
 	init_serial();
@@ -314,6 +313,7 @@ int main(void)
 # error "Select START_ condition for bootloader in main.c"
 #endif
 
+	debug_endlessTogglePin();						// XXX BLOCKING call forever, starts 327ms after power-up
 #if 0  // XXX enable after DEBUGGING
 	for(;;) {
 		val = recvchar();
