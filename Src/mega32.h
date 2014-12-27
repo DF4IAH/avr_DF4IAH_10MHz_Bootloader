2c4704587875b8eb146b4afb4861c2c349603462
Ulrich Habel <espero7757@gmx.net>
2014-12-27 10:37:23 +0000

    avr_DF4IAH_Bootloader - Hardware V1.0 --> V1.1

--- Src/mega32.h
+++ Src/mega32.h
@@ -41,16 +41,16 @@
  * Pin "STARTPIN" on port "STARTPORT" in this port has to grounded
  * (active low) to start the bootloader
  */
-#define PROBE_PORT			PORTC
-#define PROBE_DDR			DDRC
-#define PROBE_PIN			PINC
-#define PROBE_PNUM			PINC4		// JP3 BootLoader
+#define PROBE_PORT			PORTD
+#define PROBE_DDR			DDRD
+#define PROBE_PIN			PIND
+#define PROBE_PNUM			PIND3		// JP3 BootLoader
 
 /* PWM Debugging toggle pin */
-#define PWMTOGGLEPIN_DDR	DDRD
-#define PWMTOGGLEPIN_PORT	PORTD
-#define PWMTOGGLEPIN_PIN	PIND
-#define PWMTOGGLEPIN_PNUM	PIN3		// PD3(INT1) - Pin 5
+#define PWMTOGGLEPIN_DDR	DDRC
+#define PWMTOGGLEPIN_PORT	PORTC
+#define PWMTOGGLEPIN_PIN	PINC
+#define PWMTOGGLEPIN_PNUM	PIN5		// PC5(ADC5/SCL) - Pin 28
 
 /* BOOT token and place of BOOT token as offset before RAMEND */
 # define BOOT_TOKEN			0xb00f
