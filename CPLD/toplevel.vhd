66da4c0e6d72b7ac5b273d0ac37e2ccb93cb6fd6
Ulrich Habel <espero7757@gmx.net>
2017-04-11 22:45:15 +0200

    HW_V2.3.1 - CPLD - new phase compare frequency of 250kHz instead of 2.5MHz

--- CPLD/toplevel.vhd
+++ CPLD/toplevel.vhd
@@ -11,6 +11,7 @@ use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 use ieee.numeric_std.ALL;
 
+
 entity top_lev is
 	port (
 --		-- JTAG
@@ -26,9 +27,8 @@ entity top_lev is
 		-- Clock Divider
 		C_20MHZ 		: in  std_logic;
 --		C_10MHZ			: out std_logic;	-- output via C_OUT1 / C_OUT2
-		C_5MHZ			: out std_logic;
-		C_2MHZ5			: out std_logic;
 --		C_1MHZ			: out std_logic;	-- output via C_OUT2 / C_OUT1
+		C_250KHZ 		: out std_logic;
 		C_OUT1			: out std_logic;
 		C_OUT2			: out std_logic;
 		C_10KHZ 		: in  std_logic;	-- used for - HIGH: C_OUT1 <= C_10MHZ, C_OUT2 <= C_1MHZ / LOW: exchanged
@@ -41,10 +41,8 @@ entity top_lev is
 		RSFF_CLK		: out std_logic;
 
 		GATE_TRIG		: out std_logic;
---		GATE  			: in  std_logic;	-- not more used for tristate driver
 
 		PHASE_RSFF		: out std_logic;	-- directly used by diode and low pass filter
---		PHASE 			: out std_logic;	-- driver to slow for changing between Hi/Lo and Z
 
 		-- Serial Communications
 		SER_GPS			: in  std_logic;
@@ -56,10 +54,11 @@ entity top_lev is
 		MCU_RXD			: out std_logic;
 
 		-- PWM voltage convertion
-		PWM_PULL_IN		: in  std_logic;	-- 5V signal in
-		PWM_PULL_OUT		: out std_logic		-- PWM_PULL low pass filter
+		PWM_PULL_IN		: in  std_logic;	-- 5V tolerant signal in
+		PWM_PULL_OUT		: out std_logic		-- PWM_PULL 3.3V output signal for following low pass filter
 	);
 end;
+
 
 architecture structural of top_lev is
 	component clock_div  -- component declaration for clock_div
@@ -67,20 +66,15 @@ architecture structural of top_lev is
 			RESETn		: in  std_logic;
 			C_20MHZ		: in  std_logic;
 			C_10MHZ		: out std_logic;
-			C_5MHZ		: out std_logic;
-			C_2MHZ5		: out std_logic;
-			C_1MHZ		: out std_logic
+			C_1MHZ		: out std_logic;
+			C_250KHZ	: out std_logic
 		);
 	end component;
 
 	component capture  -- component declaration for capture
 		port (
 			RESETn		: in  std_logic;
---			C_20MHZ		: in  std_logic;
---			C_10MHZ		: in  std_logic;
---			C_5MHZ		: in  std_logic;
-			C_2MHZ5		: in  std_logic;
---			C_10KHZ		: in  std_logic;
+			C_250KHZ	: in  std_logic;
 			C_PPS		: in  std_logic;
 
 			GPS_REG		: out std_logic;
@@ -89,10 +83,8 @@ architecture structural of top_lev is
 			RSFF_CLK	: out std_logic;
 
 			GATE_TRIG	: out std_logic;
---			GATE		: in  std_logic;
 
 			PHASE_RSFF	: out std_logic
---			PHASE		: out std_logic
 		);
 	end component;
 
@@ -121,63 +113,61 @@ architecture structural of top_lev is
 
 -- Declaration of top_lev signals
 	signal C_10MHZ_loc		:     std_logic;
-	signal C_5MHZ_loc		:     std_logic;
-	signal C_2MHZ5_loc		:     std_logic;
 	signal C_1MHZ_loc		:     std_logic;
+	signal C_250KHZ_loc		:     std_logic;
 
 begin  --  structural description begins
 	clock_div_0: clock_div 
 		port map (
-			RESETn => RESETn,
-			C_20MHZ => C_20MHZ, 
-			C_10MHZ => C_10MHZ_loc, 
-			C_5MHZ  => C_5MHZ_loc,
-			C_2MHZ5 => C_2MHZ5_loc,
-			C_1MHZ  => C_1MHZ_loc
+			RESETn       => RESETn,
+			C_20MHZ      => C_20MHZ, 
+			C_10MHZ      => C_10MHZ_loc, 
+			C_1MHZ       => C_1MHZ_loc,
+			C_250KHZ     => C_250KHZ_loc
 		);
 
 	capture_0: capture
 		port map (
-			RESETn => RESETn,
-			C_2MHZ5 => C_2MHZ5_loc,
-			C_PPS => C_PPS,
+			RESETn       => RESETn,
+			C_250KHZ     => C_250KHZ_loc,
+			C_PPS        => C_PPS,
 
-			GPS_REG => GPS_REG,
+			GPS_REG      => GPS_REG,
 
-			SYNC => SYNC,
-			RSFF_CLK => RSFF_CLK,
+			SYNC         => SYNC,
+			RSFF_CLK     => RSFF_CLK,
 
-			GATE_TRIG => GATE_TRIG,
---			GATE => GATE,
+			GATE_TRIG    => GATE_TRIG,
 
-			PHASE_RSFF => PHASE_RSFF
---			PHASE => PHASE
+			PHASE_RSFF   => PHASE_RSFF
 		);
 
 	sercom_0 : sercom
 		port map (
-			SER_GPS => SER_GPS,
+			SER_GPS      => SER_GPS,
 
-			GPS_TXD => GPS_TXD,
-			ISP_TXD => ISP_TXD,
-			MCU_TXD => MCU_TXD,
+			GPS_TXD      => GPS_TXD,
+			ISP_TXD      => ISP_TXD,
+			MCU_TXD      => MCU_TXD,
 
-			GPS_RXD => GPS_RXD,
-			ISP_RXD => ISP_RXD,
-			MCU_RXD => MCU_RXD
+			GPS_RXD      => GPS_RXD,
+			ISP_RXD      => ISP_RXD,
+			MCU_RXD      => MCU_RXD
 		);
 
 	pwmpull_0 : pwmpull
 		port map (
-			PWM_PULL_IN => PWM_PULL_IN,
+			PWM_PULL_IN  => PWM_PULL_IN,
 			PWM_PULL_OUT => PWM_PULL_OUT
 		);
 
 	-- OUTPUT pins derived from OUTPUT&INPUT nets
-	C_5MHZ  <= C_5MHZ_loc;
-	C_2MHZ5 <= C_2MHZ5_loc;
+--	C_5MHZ    <= C_5MHZ_loc;
+--	C_2MHZ5   <= C_2MHZ5_loc;
+	C_250KHZ  <= C_250KHZ_loc;
 
 	-- C_10KHZ is no more used for new GPS modules, new function established here.
+	-- MUX C_10MHZ / C_1MHZ  --> C_OUT1 / C_OUT2
 	process (C_10KHZ, C_10MHZ_loc, C_1MHZ_loc)
 	begin
 	    if C_10KHZ = '1' then
@@ -204,50 +194,59 @@ entity clock_div is
 		RESETn			: in  std_logic;
 		C_20MHZ			: in  std_logic;
 		C_10MHZ			: out std_logic;
-		C_5MHZ			: out std_logic;
-		C_2MHZ5			: out std_logic;
-		C_1MHZ			: out std_logic
+		C_1MHZ			: out std_logic;
+		C_250KHZ		: out std_logic
 	);
 end clock_div;
 
 architecture BEHAVIORAL of clock_div is
-	signal UINT			: unsigned(2 downto 0);
-	signal UINT5			: unsigned(2 downto 0);
-	signal LOGCTR			: std_logic_vector(2 downto 0);
+	signal UINT2MHZ			: unsigned(2 downto 0);
+	signal C_10MHZ_r		: std_logic;
 	signal C_1MHZ_r			: std_logic;
+	signal C_500KHZ_r		: std_logic;
+	signal C_250KHZ_r		: std_logic;
 
-begin 
+begin
+--	Create C_10MHZ signal
 	process (RESETn, C_20MHZ)
 	begin
 	    if RESETn = '0' then
-	        UINT <= 0;
+		C_10MHZ_r <= '0';
 
 	    elsif rising_edge(C_20MHZ) then
-	        UINT <= UINT + 1;
+		C_10MHZ_r <= not C_10MHZ_r;
 	    end if;
 	end process;
 
-	process (RESETn, LOGCTR(0))
+--	Create C_1MHZ / C_500KHZ / C_250KHZ signal
+	process (RESETn, C_20MHZ, C_10MHZ_r)
 	begin
 	    if RESETn = '0' then
-		UINT5 <= 0;
-	        C_1MHZ_r <= '0';
+	        UINT2MHZ <= 0;
+		C_1MHZ_r <= '0';
+		C_500KHZ_r <= '0';
+		C_250KHZ_r <= '0';
 
-	    elsif rising_edge(LOGCTR(0)) then
-		if UINT5 = "100" then
-			UINT5 <= 0;
+	    elsif rising_edge(C_20MHZ) and (C_10MHZ_r = '1') then
+		if UINT2MHZ = "100" then
+			UINT2MHZ <= 0;
+			if (C_1MHZ_r = '1') and (C_500KHZ_r = '1') then
+				C_250KHZ_r <= not C_250KHZ_r;
+			end if;
+			if C_1MHZ_r = '1' then
+				C_500KHZ_r <= not C_500KHZ_r;
+			end if;
 			C_1MHZ_r <= not C_1MHZ_r;
 		else
-		        UINT5 <= UINT5 + 1;
+        		UINT2MHZ <= UINT2MHZ + 1;
 		end if;
 	    end if;
 	end process;
 
-	LOGCTR  <= std_logic_vector(UINT); -- type conversion
-	C_10MHZ <= LOGCTR(0);
-	C_5MHZ  <= LOGCTR(1);
-	C_2MHZ5 <= LOGCTR(2);
-	C_1MHZ	<= C_1MHZ_r;
+--	Export register states to output signals
+	C_10MHZ  <= C_10MHZ_r;
+	C_1MHZ	 <= C_1MHZ_r;
+	C_250KHZ <= C_250KHZ_r;
 end BEHAVIORAL;
 
 
@@ -262,7 +261,7 @@ use ieee.numeric_std.ALL;
 entity capture is
 	port (
 		RESETn			: in  std_logic;
-		C_2MHZ5			: in  std_logic;
+		C_250KHZ		: in  std_logic;
 		C_PPS			: in  std_logic;
 
 		GPS_REG			: out std_logic;
@@ -271,10 +270,8 @@ entity capture is
 		RSFF_CLK		: out std_logic;
 
 		GATE_TRIG		: out std_logic;
---		GATE			: in  std_logic;
 
 		PHASE_RSFF		: out std_logic
---		PHASE			: out std_logic
 	);
 end capture;
 
@@ -316,9 +313,9 @@ begin
 
 
 	-- Phase determination A
-	process (RESETn, RSFF_CLK_loc, C_2MHZ5)
+	process (RESETn, RSFF_CLK_loc, C_250KHZ)
 	begin
-	    if (RESETn = '0') or (C_2MHZ5 = '1') then
+	    if (RESETn = '0') or (C_250KHZ = '1') then
 		PHASE_RSFF_A_loc <= '0';
 
 	    elsif rising_edge(RSFF_CLK_loc) then
@@ -327,9 +324,9 @@ begin
 	end process;
 
 	-- Phase determination B
-	process (RESETn, RSFF_CLK_loc, C_2MHZ5)
+	process (RESETn, RSFF_CLK_loc, C_250KHZ)
 	begin
-	    if (RESETn = '0') or (C_2MHZ5 = '0') then
+	    if (RESETn = '0') or (C_250KHZ = '0') then
 		PHASE_RSFF_B_loc <= '0';
 
 	    elsif rising_edge(RSFF_CLK_loc) then
